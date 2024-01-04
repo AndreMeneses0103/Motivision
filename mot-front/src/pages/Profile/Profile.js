b0c16fb14b60a6df12ecced5fbea31325edebcbf
andremeneses0103 <andremeneses0103@gmail.com>
2024-01-04 20:48:27 -0300

    Fixing and cleaning profile

--- mot-front/src/pages/Profile/Profile.js
+++ mot-front/src/pages/Profile/Profile.js
@@ -9,6 +9,103 @@ import { getUser, verifyLog } from "../../services/userFetch";
 import { getAllUserVideos } from "../../services/videoFetch";
 // import { useNavigate } from "react-router-dom";
 
+function renderVideoLoading(user){
+    const numVids = user.videos ? user.videos.length : 0;
+    return(
+        <div className="profile_main">
+            <div className="username">
+                {user.profile}
+            </div>
+                <ProfilePhoto imageSrc={`data:image/png;base64,${user.photo}`}/>
+            <UserInfos num_subs={user.subscribers} num_vids={numVids}/>
+            <div className="user_videos">
+            <div className="Profile_Videos">
+                <img
+                    id="video_loading"
+                    itemID="video_loading"
+                    src="../icons/loading.gif"
+                    alt="Loading..."
+                />
+            </div>
+            </div>
+        </div>
+    );
+}
+
+function renderPhotoLoading(user){
+    const numVids = user.videos ? user.videos.length : 0;
+    return(
+        <div className="profile_main">
+            <div className="username">
+                {user.profile}
+            </div>
+                <ProfilePhoto imageSrc={"../icons/loading.gif"}/>
+            <UserInfos num_subs={user.subscribers} num_vids={numVids}/>
+            <div className="user_videos">
+            <div className="Profile_Videos">
+                <img
+                    id="video_loading"
+                    itemID="video_loading"
+                    src="../icons/loading.gif"
+                    alt="Loading..."
+                />
+            </div>
+            </div>
+        </div>
+    );
+}
+
+function renderAllLoading(){
+    return(
+        <div className="profile_main">
+            <div className="username">
+                Loading...
+            </div>
+                <ProfilePhoto imageSrc={"../icons/loading.gif"}/>
+            <UserInfos num_subs="Loading..." num_vids="Loading..."/>
+            <div className="user_videos">
+            <div className="Profile_Videos">
+                <img
+                    id="video_loading"
+                    itemID="video_loading"
+                    src="../icons/loading.gif"
+                    alt="Loading..."
+                />
+            </div>
+            </div>
+        </div>
+    );
+}
+
+//CORRIGIR DIFERENCA DOS VIDEOS VINDO DE USUARIO E OS REAIS
+function renderAllProfile(user, video){
+    const numVids = video.length;
+    return(
+        <div className="profile_main">
+            
+            <div className="username">
+                {user.profile}
+            </div>
+                <ProfilePhoto imageSrc={`data:image/png;base64,${user.photo}`}/>
+            <UserInfos num_subs={user.subscribers} num_vids={numVids}/>
+            <div className="user_videos">
+            <div className="Profile_Videos">
+                <ProfileVideos videoData={video}/>
+            </div>
+            
+            </div>
+        </div>
+    );
+}
+
+function renderError(){
+    return(
+        <div className="profile_main">
+            <h2>Nao foi possivel carregar usuario</h2>
+        </div>
+    );
+}
+
 function Profile(){
     const [user, setUser] = useState(null);
     const [video, setVideo] = useState(null);
@@ -75,91 +172,20 @@ function Profile(){
         if (error.code === "ERR_BAD_REQUEST") {
             return <h1>Erro de autenticação, realize o login novamente.</h1>;
         }
-    }else if(user === null){
+    }else if(user === null && video === null){
+        if(photoLoading && videoLoading){
+            return renderAllLoading();
+        }else{
+            return renderError();
+        }
+    }else if(video === null && user !== null){
         if(photoLoading){
-            const numVids = user.user.videos ? user.videos.length : 0;
-            return(
-                <div className="profile_main">
-                    <div className="username">
-                        {user.profile}
-                    </div>
-                        <ProfilePhoto imageSrc={"../icons/loading.gif"}/>
-                    <UserInfos num_subs={user.subscribers} num_vids={numVids}/>
-                    <div className="user_videos">
-                    <div className="Profile_Videos">
-                        <img
-                            id="video_loading"
-                            itemID="video_loading"
-                            src="../icons/loading.gif"
-                            alt="Loading..."
-                        />
-                    </div>
-                    </div>
-                </div>
-            );
+            return renderAllLoading();
         }else{
-            return <h1>Cant find the user</h1>;
+            return renderVideoLoading(user);
         }
-    }else if(video === null){
-        if(videoLoading){
-            const numVids = video.videos ? video.videos.length : 0;
-            return(
-                <div className="profile_main">
-                    
-                    <div className="username">
-                        {user.profile}
-                    </div>
-                        <ProfilePhoto imageSrc={`data:image/png;base64,${user.photo}`}/>
-                    <UserInfos num_subs={user.subscribers} num_vids={numVids}/>
-                    <div className="user_videos">
-                    <div className="Profile_Videos">
-                        <img
-                            id="video_loading"
-                            itemID="video_loading"
-                            src="../icons/loading.gif"
-                            alt="Loading..."
-                        />
-                    </div>
-                    </div>
-                </div>
-            );
-        }else{
-            const numVids = user.videos ? user.videos.length : 0;
-            return(
-                <div className="profile_main">
-                    
-                    <div className="username">
-                        {user.profile}
-                    </div>
-                        <ProfilePhoto imageSrc={`data:image/png;base64,${user.photo}`}/>
-                    <UserInfos num_subs={user.subscribers} num_vids={numVids}/>
-                    <div className="user_videos">
-                    <div className="Profile_Videos">
-                        <h2>Error to find videos...</h2>
-                    </div>
-                    </div>
-                </div>
-            );
-        }
-        
-    }else if (video !== null && user !== null){
-        const numVids = user.videos ? user.videos.length : 0;
-        return(
-            <div className="profile_main">
-                
-                <div className="username">
-                    {user.profile}
-                </div>
-                    <ProfilePhoto imageSrc={`data:image/png;base64,${user.photo}`}/>
-                <UserInfos num_subs={user.subscribers} num_vids={numVids}/>
-                <div className="user_videos">
-                <div className="Profile_Videos">
-                    <ProfileVideos videoData={video}/>
-                </div>
-                
-                </div>
-            </div>
-        );
+    }else{
+        return renderAllProfile(user, video);
     }
 }
 
