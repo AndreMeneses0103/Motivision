04a591f7dd28671b0914c52793bd23a310fe1efb
andremeneses0103 <andremeneses0103@gmail.com>
2024-01-09 18:40:07 -0300

    Add button to view the video uploader channel

--- mot-front/src/pages/Video/Video.js
+++ mot-front/src/pages/Video/Video.js
@@ -1,99 +1,106 @@
-// import { useState } from "react";
 import "../../styles/Video.css";
 // import Head from "../../components/Head";
 import AllComments from "./All_comments";
 import AllVideos from "./All_Random_Videos";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getTokenId, refreshToken,} from "../../scripts/getUser";
 import { getUser, verifyLog } from "../../services/userFetch";
 import { getVideoInfo, getVideoSource } from "../../services/videoFetch";
 // import { BrowseRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function renderLoading(){
-    return(
-        <div className="mainpage">
-        <div className="video_itens">
-        <div className="loading_space">
-            <img
-                id="video_loading"
-                itemID="video_loading"
-                src="../icons/loading.gif"
-                alt="Loading..."
-            />
-        </div>
-        </div>
-    </div>
-    );
-}
-
-function renderError(message){
-    return(
-        <div className="video_itens">
-            <h1>{message}</h1>
-        </div>
-    );
-}
-
-function renderVideo(videoData,userData,videoSource){
-    const info = videoData[0];
-    return(
-        <div className="mainpage">
-            <div className="video_itens">
-                <div className="video_channel">
-                    <div className="channel_user">
-                        <button className="channel_btn">
-                            <img
-                                id="userphoto"
-                                itemID="userphoto"
-                                src={`data:image/png;base64,${userData.photo}`}
-                                alt="Foto do Usuario"
-                            />
-                        </button>
-                        <span id="channel_name">{userData.profile}</span>
-                    </div>
-                    <div className="video_subs">
-                        <button className="subs_btn">
-                            Subscribe!
-                        </button>
-                    </div>
-                </div>
-                <div className="video_title">{info.title}</div>
-                <div className="screen_video">
-                    <video id="playing_video" controls src={videoSource} type="video/mp4"></video>
-                </div>
-                <div className="video_stats">
-                    <span className="video_icon_span"><img className="video_icons" id="view_icon" src="./icons/olho.png" alt="View"/> {info.videodata.views}</span>
-                    <span className="video_icon_span"><img className="video_icons" id="like_icon" src="./icons/afirmativo.png" alt="View"/> {info.videodata.likes}</span>
-                    <span className="video_icon_span"><img className="video_icons" id="dislike_icon" src="./icons/afirmativo.png" alt="View"/> {info.videodata.dislikes}</span>
-                </div>
-                <div className="video_text">
-                    <span className="desc_title">Description</span>
-                    <span className="desc_text">{info.description}</span>
-                    <span className="desc_title">Tags</span>
-                    <div className="hashtags_field">
-                        {(info.tags).map((item)=>(
-                            <span className="hashtag"><a href="https://www.spacejam.com/1996/">#{item}</a></span>
-                        ))}
-                    </div>
-                </div>
-            </div>
-            <div className="recommend_video">
-                <AllVideos/>
-            </div>
-            <div className="comments_field">
-                <div className="comments_title">All Comments</div>
-                <button className="add_comment_btn">Add a comment!</button>
-                <div className="all_comments">
-                    <AllComments/>
-                </div>
-            </div>
-        </div>
-    );
-}
-
 
 function Video() {
+
+    function renderLoading(){
+        return(
+            <div className="mainpage">
+            <div className="video_itens">
+            <div className="loading_space">
+                <img
+                    id="video_loading"
+                    itemID="video_loading"
+                    src="../icons/loading.gif"
+                    alt="Loading..."
+                />
+            </div>
+            </div>
+        </div>
+        );
+    }
+    
+    function renderError(message){
+        return(
+            <div className="video_itens">
+                <h1>{message}</h1>
+            </div>
+        );
+    }
+    
+    
+    const loadChannel = () => {
+        console.log("USER DATA:", userData);
+        navigate(`/profile?user=${userData.id}`);
+    };
+    
+    function renderVideo(videoData,userData,videoSource){
+        const info = videoData[0];
+        return(
+            <div className="mainpage">
+                <div className="video_itens">
+                    <div className="video_channel">
+                        <div className="channel_user">
+                            <button className="channel_btn" onClick={loadChannel}>
+                                <img
+                                    id="userphoto"
+                                    itemID="userphoto"
+                                    src={`data:image/png;base64,${userData.photo}`}
+                                    alt="Foto do Usuario"
+                                />
+                            </button>
+                            <span id="channel_name">{userData.profile}</span>
+                        </div>
+                        <div className="video_subs">
+                            <button className="subs_btn">
+                                Subscribe!
+                            </button>
+                        </div>
+                    </div>
+                    <div className="video_title">{info.title}</div>
+                    <div className="screen_video">
+                        <video id="playing_video" controls src={videoSource} type="video/mp4"></video>
+                    </div>
+                    <div className="video_stats">
+                        <span className="video_icon_span"><img className="video_icons" id="view_icon" src="./icons/olho.png" alt="View"/> {info.videodata.views}</span>
+                        <span className="video_icon_span"><img className="video_icons" id="like_icon" src="./icons/afirmativo.png" alt="View"/> {info.videodata.likes}</span>
+                        <span className="video_icon_span"><img className="video_icons" id="dislike_icon" src="./icons/afirmativo.png" alt="View"/> {info.videodata.dislikes}</span>
+                    </div>
+                    <div className="video_text">
+                        <span className="desc_title">Description</span>
+                        <span className="desc_text">{info.description}</span>
+                        <span className="desc_title">Tags</span>
+                        <div className="hashtags_field">
+                            {(info.tags).map((item)=>(
+                                <span className="hashtag"><a href="https://www.spacejam.com/1996/">#{item}</a></span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="recommend_video">
+                    <AllVideos/>
+                </div>
+                <div className="comments_field">
+                    <div className="comments_title">All Comments</div>
+                    <button className="add_comment_btn">Add a comment!</button>
+                    <div className="all_comments">
+                        <AllComments/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+
     const [videoData, setVideoData] = useState(null);
     const [videoSource, setVideoSource] = useState(null);
     const [userData, setUserData] = useState(null);
@@ -101,6 +108,7 @@ function Video() {
     const [dataLoading, setDataLoading] = useState(true);
     const [userLoading, setUserLoading] = useState(true);
     const [videoLoading, setVideoLoading] = useState(true);
+    const navigate = useNavigate();
 
     const local = useLocation();
     const params = new URLSearchParams(local.search);
@@ -189,6 +197,8 @@ function Video() {
             setVideoLoading(false);
         }
     }
+
+
 
     useEffect(()=>{
         reset();
