c3f5ff25f73489e18dafe03f414e26ffe3051692
andremeneses0103 <andremeneses0103@gmail.com>
2024-02-08 15:29:32 -0300

    Creating Controllers

---
+++ mot-back/controllers/UserController.ts
@@ -0,0 +1,200 @@
+import UserDAO from "../models/DAO/UserDAO";
+import { Request, Response } from "express";
+import Permission from "../middlewares/permission";
+
+class UserController{
+    private userDao: UserDAO;
+
+    constructor(userDao: UserDAO){
+        this.userDao = userDao;
+    }
+
+    async getAllUsers(req: Request, res: Response){
+        try{
+            const access = req.headers.authorization;
+            const refresh = req.headers['refresh-token'];
+            if (access && refresh) {
+                const pm = new Permission();
+                const isValid = await pm.getPermission(`${access}, ${refresh}`);
+                if(isValid.auth === true){
+                    let response = {};
+                    if(isValid.newAccessToken){
+                        response = {isValid}
+                    }else{
+                        let allUsers = await this.userDao.getAllUsers();
+                        response = {allUsers};
+                    }
+                    if(!response){
+                        res.status(404).json("No users found");
+                    }else{
+                        res.status(200).json(response);
+                    }
+                }else{
+                    res.status(401).json({auth:false, message:"Both tokens are not valid. Please login again."});
+                }
+            }else{
+                return res
+                    .status(401)
+                    .json({ auth: false, message: "Tokens not found" });
+            }
+        }catch(error){
+            console.error(error);
+            res.status(500).json({error: "An error occurred in server."})
+        }
+        
+    }
+
+    async postUserByCredentials(req: Request, res: Response){
+        try{
+            const {name, password} = req.body;
+            const user = await this.userDao.postUserByCredentials(name,password);
+            if(user){
+                res.status(200).json(user);
+            }else{
+                res.status(404).json({message:"User not found"})
+            }
+        }catch(error){
+            console.error(error);
+            res.status(500).json({error:"An error occurred in server."})
+        }
+    }
+
+    async getUserByEmail(req: Request, res: Response){
+        try{
+            const access = req.headers.authorization;
+            const refresh = req.headers['refresh-token'];
+            if(access && refresh){
+                const pm = new Permission();
+                const isValid = await pm.getPermission(`${access}, ${refresh}`);
+                if(isValid.auth === true){
+                    const email = req.query.email as string;
+                    const users = await this.userDao.getUserByEmail(email);
+                    if(users){
+                        res.status(200).json(users);
+                    }else{
+                        res.status(404).json({message: "User not found"});
+                    }
+                }else{
+                    res.status(401).json({auth:false, message:"Both tokens are not valid. Please login again."});
+                }
+            }else{
+                return res
+                    .status(401)
+                    .json({ auth: false, message: "Tokens not found" });
+            }
+        }catch (error){
+            console.error(error);
+            res.status(500).json({error:"An error occurred in server."})
+        }
+        
+    }
+
+    async getUsersByIds(req: Request, res: Response){
+        try{
+            const access = req.headers.authorization;
+            const refresh = req.headers['refresh-token'];
+            const userSelected = req.query.users as string;
+            const allUsers = userSelected.split(",");
+            if (access && refresh) {
+                const pm = new Permission();
+                const isValid = await pm.getPermission(`${access}, ${refresh}`);
+                if(isValid.auth === true){
+                    let response = {};
+                    if(isValid.newAccessToken){
+                        response = {isValid}
+                    }else{
+                        // console.log(isValid.value);
+                        const user = await this.userDao.getUsersByIds(allUsers);
+                        response = {user};
+                    }
+                    if(response){
+                        res.status(200).json(response);
+                    }else{
+                        res.status(404).json({message:"User not found"});
+                    }
+                }else{
+                    res.status(401).json({auth:false, message:"Both tokens are not valid. Please login again."});
+                }
+            }else{
+                return res
+                    .status(401)
+                    .json({ auth: false, message: "Tokens not found" });
+            }
+        }catch(error){
+            console.error(error);
+            res.status(500).json({error:"An error occurred in server."})
+        }
+    }
+
+    async getUserById(req: Request, res: Response){
+        try{
+            const access = req.headers.authorization;
+            const refresh = req.headers['refresh-token'];
+            const userSelected = req.query.user as string;
+            if (access && refresh) {
+                const pm = new Permission();
+                const isValid = await pm.getPermission(`${access}, ${refresh}`);
+                if(isValid.auth === true){
+                    let response = {};
+                    if(isValid.newAccessToken){
+                        response = {isValid}
+                    }else{
+                        // console.log(isValid.value);
+                        const user = await this.userDao.getUserById(userSelected);
+                        response = {user};
+                    }
+                    if(response){
+                        res.status(200).json(response);
+                    }else{
+                        res.status(404).json({message:"User not found"});
+                    }
+                }else{
+                    res.status(401).json({auth:false, message:"Both tokens are not valid. Please login again."});
+                }
+            }else{
+                return res
+                    .status(401)
+                    .json({ auth: false, message: "Tokens not found" });
+            }
+        }catch(error){
+            console.error(error);
+            res.status(500).json({error: "An error occurred in server."})
+        }
+    }
+
+    async getUserByName(req:Request, res: Response){
+        try{
+            const access = req.headers.authorization;
+            const refresh = req.headers['refresh-token'];
+            const nome = req.query.name as string;
+            if (access && refresh){
+                const pm = new Permission();
+                const isValid = await pm.getPermission(`${access}, ${refresh}`);
+                if(isValid.auth === true){
+                    let response = {};
+                    if(isValid.newAccessToken){
+                        response = {isValid}
+                    }else{
+                        // console.log(isValid.value);
+                        const user = await this.userDao.getUserByName(nome);
+                        response = {user};
+                    }
+                    if(response){
+                        res.status(200).json(response);
+                    }else{
+                        res.status(404).json({message:"User not found"});
+                    }
+                }
+            }
+        }catch(error){
+            console.error(error);
+            res.status(500).json({error:"An error occurred in server."})
+        }
+    }
+
+    //CONTINUAR PASSAR CONTROLLERS DO ROUTES PARA CA
+}
+
+export{
+    UserController
+}
--- mot-back/routes/userRouter.ts
+++ mot-back/routes/userRouter.ts
@@ -2,128 +2,31 @@ import express, { Router, Request, Response } from "express";
 import UserDAO from "../models/DAO/UserDAO";
 import Database from "../database";
 import Permission from "../middlewares/permission";
+import { UserController } from "../controllers/UserController";
 
 export default class userRouter {
     private route: Router;
+    private controller: UserController;
     private data: UserDAO;
     constructor(database: Database) {
         this.route = express.Router();
+        this.controller = new UserController(new UserDAO(database));
         this.data = new UserDAO(database);
         this.configRouter();
     }
     private configRouter(): void {
-        this.route.get("/all", async (req: Request,res:Response)=>{
-            const access = req.headers.authorization;
-            const refresh = req.headers['refresh-token'];
-            if (access && refresh) {
-                const pm = new Permission();
-                const isValid = await pm.getPermission(`${access}, ${refresh}`);
-                if(isValid.auth === true){
-                    let response = {};
-                    if(isValid.newAccessToken){
-                        response = {isValid}
-                    }else{
-                        let allUsers = await this.data.getAllUsers();
-                        response = {allUsers};
-                    }
-                    res.json(response);
-                }else{
-                    res.status(401).json({auth:false, message:"Both tokens are not valid. Please login again."});
-                }
-            }else{
-                return res
-                    .status(401)
-                    .json({ auth: false, message: "Tokens not found" });
-            }
-        });
+        this.route.get("/all", this.controller.getAllUsers.bind(this.controller));
 
-        this.route.post("/postUserCredentials",async (req:Request, res:Response) => {           
-            const {name, password} = req.body;
-            const user = await this.data.postUserByCredentials(name,password);
-            res.json(user);
-        });
+        this.route.post("/postUserCredentials", this.controller.postUserByCredentials.bind(this.controller));
 
-        this.route.get("/getEmailInfo", async (req: Request,res:Response)=>{
-            const access = req.headers.authorization;
-            const refresh = req.headers['refresh-token'];
-            if(access && refresh){
-                const pm = new Permission();
-                const isValid = await pm.getPermission(`${access}, ${refresh}`);
-                if(isValid.auth === true){
-                    const email = req.query.email as string;
-                    const users = await this.data.getUserByEmail(email);
-                    res.json(users);
-                }else{
-                    res.status(401).json({auth:false, message:"Both tokens are not valid. Please login again."});
-                }
-            }else{
-                return res
-                    .status(401)
-                    .json({ auth: false, message: "Tokens not found" });
-            }
-            
-        });
+        //PASSAR O RESTO
+        this.route.get("/getEmailInfo", this.controller.getUserByEmail.bind(this.controller));
 
-        this.route.get("/getIdsInfos", async(req: Request, res:Response)=>{
-            const access = req.headers.authorization;
-            const refresh = req.headers['refresh-token'];
-            const userSelected = req.query.users as string;
-            const allUsers = userSelected.split(",");
-            if (access && refresh) {
-                const pm = new Permission();
-                const isValid = await pm.getPermission(`${access}, ${refresh}`);
-                if(isValid.auth === true){
-                    let response = {};
-                    if(isValid.newAccessToken){
-                        response = {isValid}
-                    }else{
-                        // console.log(isValid.value);
-                        const user = await this.data.getUsersByIds(allUsers);
-                        response = {user};
-                    }
-                    res.json(response);
-                }else{
-                    res.status(401).json({auth:false, message:"Both tokens are not valid. Please login again."});
-                }
-            }else{
-                return res
-                    .status(401)
-                    .json({ auth: false, message: "Tokens not found" });
-            }
-        })
+        this.route.get("/getIdsInfos", this.controller.getUsersByIds.bind(this.controller));
 
-        this.route.get("/getIdInfo", async (req: Request,res:Response)=>{
-            const access = req.headers.authorization;
-            const refresh = req.headers['refresh-token'];
-            const userSelected = req.query.user as string;
-            if (access && refresh) {
-                const pm = new Permission();
-                const isValid = await pm.getPermission(`${access}, ${refresh}`);
-                if(isValid.auth === true){
-                    let response = {};
-                    if(isValid.newAccessToken){
-                        response = {isValid}
-                    }else{
-                        // console.log(isValid.value);
-                        const user = await this.data.getUserById(userSelected);
-                        response = {user};
-                    }
-                    res.json(response);
-                }else{
-                    res.status(401).json({auth:false, message:"Both tokens are not valid. Please login again."});
-                }
-            }else{
-                return res
-                    .status(401)
-                    .json({ auth: false, message: "Tokens not found" });
-            }
-        });
+        this.route.get("/getIdInfo", this.controller.getUserById.bind(this.controller));
 
-        this.route.get("/getNameInfo", async (req: Request,res:Response)=>{
-            const nome = req.query.name as string;
-            const users = await this.data.getUserByName(nome);
-            res.json(users);
-        });
+        this.route.get("/getNameInfo",this.controller.getUserByName.bind(this.controller));
     }
 
     public getRouter(): Router{
