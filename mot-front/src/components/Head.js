6784879292946ba7bc47e052f6becaad2b271f5a
andremeneses0103 <andremeneses0103@gmail.com>
2024-08-30 13:57:55 -0300

    User bubble options added

--- mot-front/src/components/Head.js
+++ mot-front/src/components/Head.js
@@ -1,30 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Head.css";
 import { useUser } from "../contexts/UserContext";
 
 function Head() {
-    // const [userData, setUserData] = useState([]);
     const {user, loading, error} = useUser();
     const navigate = useNavigate();
+    const [showOptions, setShowOptions] = useState(false);
 
     const loadUpload = () => {
         navigate("/upload");
+        setShowOptions(false);
     };
 
     const loadProfile = () => {
         navigate(`/profile?user=${user.usersettings.userid}`);
+        setShowOptions(false);
     };
 
     const loadMain = () => {
         navigate("/main");
+        setShowOptions(false);
+    };
+
+    const logout = () => {
+        
+    };
+
+    const toggleOptions = () => {
+        setShowOptions(!showOptions);
     };
 
     if(user){
         return (
             <div className="header">
                     <div className="user" id="user">
-                        <button className="btn_profile" onClick={loadProfile}>
+                        <button className="btn_profile" onClick={toggleOptions}>
                         {loading ? (
                             <img
                                 id="userphoto"
@@ -40,9 +51,15 @@ function Head() {
                                 alt="User Profile"
                             />
                         )}
-                            
+                        
                         </button>
                     </div>
+                    {showOptions && (
+                        <div className="balloon">
+                            <button onClick={loadProfile}>View Profile</button>
+                            <button onClick={logout}>Logout</button>
+                        </div>
+                    )}
                 <div className="logo">
                     <button className="btn_logo" onClick={loadMain}>
                         Motivision
